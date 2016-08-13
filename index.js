cfafe12150180ed1c71dcee3721fda448b5bb34d
谭翔 <510754787@qq.com>
2016-08-13 12:46:04 +0800

    second

--- index.js
+++ index.js
@@ -1,233 +1,289 @@
 $(function(){
-    //动画的练习
-
-	// //animate():动画
-	// //queue()队列
-	// //dequeue():出对
-	// //delay()：延迟时间
-	// //stop();
-	// $(".animation")
-	// .animate({width:400},1000)
-	// .delay(1000)
-	// .queue(function(){
-	// 	$(this).css('backgroundColor',"yellow").dequeue();
-	// })
-	// .animate({height:400},1000)
-
-	//子弹的练习
- //    for(var i=0;i<1000;i++){
-	//     var w=Math.floor(Math.random()*4+3);
-	//     var b=Math.floor(Math.random()*100+155);
-	//     var left=Math.floor(Math.random()*$(document).width());
-	//     var top=Math.floor(Math.random()*$(document).height());
-	// 	$("<div>")
-	// 	.addClass("zidan")
-	// 	.width(w)
-	// 	.height(w)
-	// 	.css({
-	// 		backgroundColor:'rgba(41,41,'+b+',0.4)'
-	// 	})
-	// 	.appendTo('body')
-	// 	.delay(i*10)
-	// 	.animate({
-	// 		left:left,
-	// 		top:top
-	// 	})
-	// }
-
-    
-    //console.table(poker)
-    //var biao2=[{1:"1"}, {2:"2"}, {3:"3"}, {4:"4"}, {5:"5"}, {6:"6"}, {7:"7"}, {8:"8"}, {9:"9"}, {10:"11"}, {11:"12"}, {12:"13"}, {13:"14"}]
-
-    
-    //设置牌的随机数
-    var poker=[];
-    var colors=["c","h","d","s"];
-    var biao=[];
-    while (poker.length < 52 ){
-        //设定牌的花色
-        var huaSe=colors[Math.floor(Math.random()*4)];
-        //设定牌的数字
-        var shuZi=Math.ceil(Math.random()*13);
-        var item={huaSe:huaSe, shuZi:shuZi};
-        //去掉重复的牌,
-        if(!biao[huaSe+'-'+shuZi]){
-            poker.push(item);
-            biao[huaSe+"-"+shuZi]=true;
-        }
+   //var poker=[{colors:'c',number:'num'}];
+  var tishi=$('.zuozi .tishi');
+  var xiezi=$('.tishi .title')
+  var chahao=$('.chahao')
+  var queren=$('.queren')
+  var delay=function(){
+       tishi.css({'display':'none'}).animate({opacity:0})
     }
+  chahao.on('click',function(){delay()})
+  queren.on('click',function(){delay()})
+   var warn=function(){    
+    tishi.css({'display':'block'}).animate({opacity:1})
+    setTimeout(delay,3000)
+   }
 
+   // 计时器
+   var $jishi=$('.time .font');
+   var time=0; 
+   var min=0;
+   var second=0;
+   var state;
+  function jishi(){
+     if(state=="over"){
+      time = 0;
+      min=0;
+      second=0;
+      $jishi.html("00:00");
+    }
+    time +=1; 
+    second=time%60;
+    if(time%60 == 0){
+      min = parseInt(min);
+      min += 1;
+      min = (min<10)?'0'+min:min;
+    }
+    second = (second<10)?'0'+second:second;
+    $jishi.html(min +':'+second);
+    state="play"
+  }
 
-    //设置行数/列数/牌的动画
-    var index=0;
-    //点击发牌
-    $(".faPai").on("click",function(){
-        for(var i=0;i<7;i++){                  //行数
-            for (var j=0;j<i+1;j++){           //列数
-                index+=1;
-                $('<div>')
-                    .addClass("pai shang")
-                    .attr("id",i+"-"+j)
-                    .data("shuZi",poker[index].shuZi)
-                    .appendTo(".zhuozi")
-                    .css({
-                        backgroundImage:"url(./image/"+poker[index].shuZi+poker[index].huaSe+".png)"
-                    })
-                    .delay(index*30)
-                    .animate({
-                        top:i*50,
-                        left:(6-i)*50+j*100,
-                        opacity:1
-                    })
-            }
+  var t=setInterval(jishi,1000)
+  clearInterval(t);
+  // 得分
+  var defen=0;
+  var $fenshu=$('.score .font');
+  var fenshu=function(val){
+    $fenshu.html(val)
+  }
+  var cli=0;
+  var star= function (){
+     state="over"
+   var poker=[];
+   var colors=['c','h','d','s'];
+   var biao={};
+   while(poker.length !== 52){
+      var num=Math.ceil(Math.random()*13);
+      var huase=colors[Math.floor(Math.random()*4)];   
+      if(!biao[huase+'_'+num]){  
+         poker.push({color:huase,number:num});        
+         biao[huase+'_'+num]=true;
+      }
+   }
+   //扑克的 动画
+   var d=0;
+   var number1={
+     1:'A',
+     2:'2',
+     3:'3',
+     4:'4',
+     5:'5',
+     6:'6',
+     7:'7',
+     8:'8',
+     9:'9',
+    10:'T',
+    11:'J',
+    12:'Q',
+    13:'K'
+   }
+    var index=-1;
+    var items=[];
+
+ 
+   for(var i=0;i<7;i++){
+      var t=i*30+25;             
+      for(var j=0;j<(i+1);j++){
+         index += 1;
+         items=poker[index];
+          d += 60;
+         var l=(6-i)*40+80*j+25;
+         $('<div>')
+         .addClass('pai shang')
+         .css({'background-image':'url(image/'+number1[poker[index].number]+poker[index].color+'.png)'})
+         .attr('id',i+'_'+j)
+         .data('shu',items.number)
+         .appendTo('.zuozi')
+         .delay(d)
+         .animate({
+            top:t,
+            left:l,
+            opacity:1
+          })
+      }      
+   }
+  
+    //管理剩余的牌
+   for(;index<poker.length;index++){    
+      items=poker[index];
+       $('<div>')
+         .addClass('pai zuo')
+         .css({'background-image':'url(image/'+number1[poker[index].number]+poker[index].color+'.png)'})
+         .data('shu',items.number)
+         .appendTo('.zuozi')
+         .delay(index*60)
+         .animate({
+            top:330,
+            left:150,
+            opacity:1
+          })}
+    //判断是否有牌
+   var zhe=function(el){
+      var x=Number($(el).attr('id').split('_')[0]);
+      var y=Number($(el).attr('id').split('_')[1]);   
+      return $('#'+(x+1)+'_'+y).length||$('#'+(x+1)+'_'+(y+1)).length;
+
+   }
+
+   // 点击牌
+   var last;
+   $('.pai').on('click',function(){ 
+   //判断游戏是否结束
+       var arr1=[];
+       var over=0;
+       $('.shang').each(function(i,ele){
+          if(!zhe(ele)){
+            arr1.push($(ele).data('shu'));
+          }
+        });
+       for(var i;i<arr1.length;i++){
+        if(arr1[i]===13){
+          over += 1;
         }
-        //给下面添加牌
-        var d=0;
-        for(;index < poker.length; index++){
-            d+=50;
-            $("<div>")
-                .addClass("pai left")
-                .attr("id",i+"-"+j)
-                .data("shuZi",poker[index].shuZi)
-                .css({
-                    backgroundImage:"url(./image/"+poker[index].shuZi+poker[index].huaSe+".png)"
-                })
-                .appendTo(".zhuozi")
-                .delay(d)
-                .animate({
-                    top:460,
-                    left:80,
-                    opacity:1
-                })
+        for(var j;j<arr1.length;j++){
+          if(arr1[i]+arr1[j]==13){
+            over += 0.5
+          }
         }
-    })
-    
-        //判断点击的牌是否被压住的函数；
-        var killed = function(e){
-            var x=Number($(e).attr("id").split("-")[0]);
-            var y=Number($(e).attr("id").split("-")[1]);
-            return $("#"+(x+1)+"-"+y).length || $("#"+(x+1)+"-"+(y+1)).length;
+       }
+
+       $('.zuo,.you').each(function(e,elem){
+        if($(elem).data('shu')===13){
+          over+=1;
         }
+        for(var j=0;j<arr1.length;j++){
+        if(arr1[j]+$(elem).data('shu')===13){
+          over+=1;
+        }
+       }})
 
-        var pervious=null;
-        $(".zhuozi .pai").on('click',function(){
-            // //判断点击的牌是否有上一张牌的类名并且被被压住 
-            // if($(this).hasClass('pervious') && killed(this)){
-            //     return;
-            // }
-            // //判断是否是K的情况；
-            // if($(this).data("shuZi")===13){
-            //     $(this).animate({
-            //         top:0,
-            //         left:700,
-            //         opacity:0
-            //     }).queue(function(){
-            //         $(this).remove();
-            //     })
-            //     return;
-            // }
-
-            // //被选中的牌设置边框和点击时上移
-            // $(this).toggleClass("chulie");
-            // if($(this).hasClass('chulie')){
-            //     $(this).animate({top:'-=30'});
-            // }else{
-            //     $(this).animate({top:'+=30'});
-            // }
-
-            // //第一次点击
-            // if(!pervious){
-            //     pervious=$(this);
-            // }else{
-            //     //第二次点击
-            //     if((pervious.data("shuZi") + $(this).data("shuZi"))===13){
-            //         $(".chulie").animate({
-            //                 top:0,
-            //                 left:700,
-            //                 opacity:0
-            //             }).queue(function(){
-            //                  $(".chulie").remove();
-            //             })
-            //         pervious=null;
-            //     }else{
-            //         $(".chulie")
-            //             .removeClass("chulie")
-            //             .animate({
-            //                 top:"+=30",
-            //             })
-            //         pervious=null;
-            //     }
-            // }
-        })
-
-        //换牌的按钮
-        var zIndex=0;
-        $(".zhuozi .change").on("click",function(){
-            zIndex++;
-           $(".pai.left")
-               .eq(-1)
-               .css({zIndex:zIndex})
-               .removeClass("left")
-               .addClass("right")
-               .animate({
-                   top:460,
-                   left:530
-               })
-        })
-
-        //所有牌返回的次数
-        var time=0;
-        $(".zhuozi .return").on("click",function(){
-            time +=1;
-            if($(".pai.left").length){
-                return;
+       if(over==0){
+                 xiezi.html("游戏结束")
+                warn();
+                againgame();
+                clearInterval(t);
             }
-            if(time>3){
-                $(".gameOver img")
-                .css({
-                    transform:"translateY(0)"
-                });
-                return;
+
+          $('.zongpeidui .font').html(over);
+      if($(this).hasClass('shang')&&zhe(this)){
+          return;
+      }else{
+       var pei=0;
+        var mm=$(this)
+        // 判断  能够配比的数目
+        $('.shang').each(function(i,ele){
+          if(!zhe($('.shang')[i])&&($(ele).data('shu')+mm.data('shu')===13)){ pei+=1;}
+        })
+        $('.zuo,.you').each(function(e,elem){
+          if($(elem).data('shu')+mm.data('shu')===13){ pei+=1;}
+        })
+        $('.kepeidui .font').html(pei);
+
+           // 点击的动画
+           $(this).toggleClass('chulie');
+           if($(this).hasClass('chulie')){
+              $(this).addClass('chulie').animate({'top':'-=20'})
             }
-            $(".pai.right").each(function(i,el){
-                $(this)
-                    .removeClass("right")
-                    .addClass("left")
-                    .delay(i*30)
-                    .animate({
-                        top:460,
-                        left:80
-                    })
-                    .css({zIndex:0})
-            })
-        })
-    
-    
+          if($(this).data('shu')===13){
+            defen+=10;
+            fenshu(defen)
 
-    //开始按钮
-    $(".screen").on("click",function(){
-        $(".screen .leftScreen").css({transform:"translateX(-500%)"}) 
-        $(".screen .rightScreen").css({  transform:"translateX(500%)"}) 
-        $(".screen .start").animate({top:"-500%"})
-        $(this).css({zIndex:0})
-        $(".operate").animate({opacity:1})
+             $(this).animate({top:0,left:375,opacity:0}).queue(function(){
+                      $(this).remove();
+               });
+             return;
+          }   
+          // 点第一张
+          if(!last){
+            last=$(this);//给第一张赋值
+            //点第二张
+          }else{
+            var current=$(this);
+            // if(last.attr('id')==current.attr('id')){
+            //    $(this).animate({'top':'+=20'})
+            //    $('.chulie').removeClass('chulie');
+            // }
+            // 两张牌
+            if(last.data('shu')+current.data('shu')===13){
+              defen+=20;
+              fenshu(defen)
+              //做完动画必须用队列移除
+               last.delay(400).animate({top:0,left:500,opacity:0}).queue(function(){
+                      $(this).remove();
+               });
+               current.animate({top:0,left:500}).queue(function(){
+                     $(this).remove();
+               });        
+              }else{
+               last.delay(400).animate({top:'+=20'})
+               current.animate({top:'+=20'})
+               $('.chulie').removeClass('chulie');
+                }
+                last=null;
+        }  
+      }
+   })
+     // 点击往右移动
+     var indexs=0;
+     $('.move-right').on('click',function(){
+      indexs+=1;
+      $('.pai.zuo').eq(-1).removeClass('zuo').animate({
+         left:375
+      }).css({
+         zIndex:indexs,
+       }).addClass('you');  
     })
-    
+    // 点击往左移动
+     $('.move-left').on('click',function(){
+      if($('.pai.zuo').length>0){
+        xiezi.html("不能点击，请等待左边牌发完！！")
+        warn();
+         return;
+      }else{
+      if(cli>3){
+        xiezi.html("已超过三次，不能换牌了！！")
+        warn();
+         return;
+      }else{cli+=1;
+      $('.zuozi .you').each(function(i,el){
+        
+         $(this).delay(60*i).css({zIndex:0}).removeClass('you').addClass('zuo')
+         .animate({left:150})
+      })}}
+     })
+  }
+  // 点击开始游戏
+  $('.meau .stargame').on('click',function(){
 
-    //鼠标划上按钮的效果
-    $(".zhuozi .change, .zhuozi .return").on("mouseover",function(){
-        $(this).css({
-            transform:"scale(1.2,1.2)"
-        })
-    })
-    $(".zhuozi .change, .zhuozi .return").on("mouseout",function(){
-        $(this).css({
-            transform:"scale(1,1)"
-        })
-    })
-
-    //点击重新开始
-    $(".restart").on("click",function(){
-        window.location.reload();
-    })
+      if($('.pai').length){
+        xiezi.html("正在进行游戏，请点击重新开始，此提示三秒后自动消失!!")
+        warn();
+        return
+      }else{ star() 
+      t=setInterval(jishi,1000)      
+       }
+  })
+  var againgame=function(){
+     $(' .zuozi .pai').remove();
+  }
+  $('.meau .againgame').on('click',function(){
+      againgame();
+      clearInterval(t);
+      $jishi.html("00:00")
+      t=setInterval(jishi,1000)
+      star();
+  })
+   var dianji=0;
+  $('.youxijieshao').on('click',function(){
+    dianji+=1;    
+    if(dianji%2==0){$('.jieshao').show(2000)}
+      else{
+    $('.jieshao').hide(2000)
+    }
+  })
+  $('.closew').on('click',function(){
+    alert('你即将离开游戏！！！')
+    window.close();
+  })  
 })
